1e7538b891c8dbd5c70c3725880f0ecb0198cf0a
Rikin Pithadia <158561699+rikin-2911@users.noreply.github.com>
2024-05-11 22:17:34 +0530

    Update and rename firstcode.py to firstcode(Hello"World").py

---
+++ firstcode(Hello"World").py
@@ -0,0 +1,12 @@
+while True:
+    user = input("Enter the name(press r if out): ")
+    if user != 'r':
+    
+     X = "Hello, "
+     X =  X + user + "!!!"
+     print(X)
+    else :
+         break
+print("completed")
+
+""" First code of python, started with hello world in some diffferent manner( use of loop) """
